0c9f9eb0d67586041b4d7846e40ef28c06cc426a
Beatriz Salles <beatrizsallesss@gmail.com>
2021-11-28 14:40:38 -0300

    chore: Create appbar

---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> { 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculadora de IMC'),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh))
+        ],
+      ),
+    );
+ }
+}
