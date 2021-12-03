91ec2fa3ea705a2f7f32ea7abef25f62415b02f7
Beatriz Salles <beatrizsallesss@gmail.com>
2021-12-02 23:40:01 -0300

    chore: Add imc logic

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  String _infoText = 'Informe seus dados';
+
+  void _resetFields() {
+    weightController.text = "";
+    heightController.text = "";
+    _infoText = "Informe seus dados";
+  }
+
+  void _calculateIMC() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text);
+      double imc = weight / (height * height);
+
+      print(imc);
+
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9){
+        _infoText = "Peso deal(${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9){
+        _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40){
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +51,7 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.greenAccent,
         actions: <Widget>[
-          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh))
+          IconButton(onPressed: _resetFields, icon: const Icon(Icons.refresh))
         ],
       ),
       backgroundColor: Colors.white,
@@ -51,6 +86,8 @@ class _HomeState extends State<Home> {
                           Radius.circular(50.0),
                         )),
                     child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: weightController,
                       textAlign: TextAlign.right,
                       decoration: InputDecoration(
                         contentPadding: const EdgeInsets.all(20.0),
@@ -107,6 +144,8 @@ class _HomeState extends State<Home> {
                           Radius.circular(50.0),
                         )),
                     child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: heightController,
                       textAlign: TextAlign.right,
                       decoration: InputDecoration(
                         hintText: "0,00",
@@ -139,7 +178,7 @@ class _HomeState extends State<Home> {
             Container(
               margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: _calculateIMC,
                 child: const Padding(
                   padding: EdgeInsets.all(10.0),
                   child: Text(
@@ -154,35 +193,36 @@ class _HomeState extends State<Home> {
                 ),
               ),
             ),
-            const Padding(
-              padding:  EdgeInsets.all(8.0),
-              child:  Text("VOCÊ ESTÁ", style: TextStyle(
-                color: Colors.black26,
-                fontSize: 18,
-              ),
-              ),
-            ),
-            const Padding(
-              padding:  EdgeInsets.all(16.0),
-              child:  Text("SAUDÁVEL", style: TextStyle(
-                color: Colors.greenAccent,
-                fontSize: 26,
-              ),),
-            ),
-            const Padding(
-              padding:  EdgeInsets.all(4.0),
-              child:  Text('SEU PESO IDEAL É ENTRE', style: TextStyle(
-                color: Colors.black26,
-                fontSize: 18,
-              ),),
-            ),
-            const Padding(
-              padding:  EdgeInsets.all(8.0),
-              child:  Text('50 kg a 60 kg', style: TextStyle(
-                color: Colors.greenAccent,
-                fontSize: 22,
-              ),),
-            ),
+            // const Padding(
+            //   padding:  EdgeInsets.all(8.0),
+            //   child:  Text("VOCÊ ESTÁ", style: TextStyle(
+            //     color: Colors.black26,
+            //     fontSize: 18,
+            //   ),
+            //   ),
+            // ),
+            // const Padding(
+            //   padding:  EdgeInsets.all(16.0),
+            //   child:  Text("SAUDÁVEL", style: TextStyle(
+            //     color: Colors.greenAccent,
+            //     fontSize: 26,
+            //   ),),
+            // ),
+            // const Padding(
+            //   padding:  EdgeInsets.all(4.0),
+            //   child:  Text('SEU PESO IDEAL É ENTRE', style: TextStyle(
+            //     color: Colors.black26,
+            //     fontSize: 18,
+            //   ),),
+            // ),
+            // const Padding(
+            //   padding:  EdgeInsets.all(8.0),
+            //   child:  Text('50 kg a 60 kg', style: TextStyle(
+            //     color: Colors.greenAccent,
+            //     fontSize: 22,
+            //   ),),
+            // ),
+            Text(_infoText)
           ],
         ),
       ),
