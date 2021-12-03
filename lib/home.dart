8102b2aa5306fcc217af127f11b4f9292e54965c
Beatriz Salles <beatrizsallesss@gmail.com>
2021-12-02 22:13:14 -0300

    chore: Add inputs

--- lib/home.dart
+++ lib/home.dart
@@ -7,7 +7,7 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> { 
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +19,118 @@ class _HomeState extends State<Home> {
           IconButton(onPressed: () {}, icon: const Icon(Icons.refresh))
         ],
       ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 8.0, 8.0, 0.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text(
+                  "Peso",
+                  style: TextStyle(
+                    color: Colors.greenAccent,
+                    fontSize: 24,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(5.0),
+                    margin: const EdgeInsets.all(30.0),
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 0.2,
+                            blurRadius: 14,
+                            offset: const Offset(4, 8),
+                          ),
+                        ],
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(50.0),
+                        )),
+                    child: TextField(
+                      textAlign: TextAlign.right,
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.all(20.0),
+                        hintText: "1,00",
+                        hintStyle: const TextStyle(fontSize: 20.0, color: Colors.black26),
+                        suffixIcon: const Text('KG', style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.greenAccent,
+                        ),),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text(
+                  "Altura",
+                  style: TextStyle(
+                    color: Colors.greenAccent,
+                    fontSize: 24,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(5.0),
+                    margin: const EdgeInsets.all(30.0),
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 0.2,
+                            blurRadius: 14,
+                            offset: const Offset(4, 8),
+                          ),
+                        ],
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(50.0),
+                        )),
+                    child: TextField(
+                      textAlign: TextAlign.right,
+                      decoration: InputDecoration(
+                        hintText: "1,00",
+                        hintStyle: const TextStyle(fontSize: 20.0, color: Colors.black26),
+                        suffixIcon: const Text('M', style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.greenAccent,
+                        ),),
+                        contentPadding: const EdgeInsets.all(20.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
- }
+  }
 }
